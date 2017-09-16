f69be11c9c192d409e834f5b3ceb98442e85b262
PKUGoodSpeed <pkulizebo@gmail.com>
2017-09-16 01:12:43 +0000

    sdf

--- TFProjects/CNN/main2.py
+++ TFProjects/CNN/main2.py
@@ -41,12 +41,35 @@ fc_relu_list = [True, True]
 
 if __name__ == '__main__':
     mnist_plot = TFPlots.MnistImagePlot(height = img_size, width = img_size)
+    
+    '''
     test_model = MultiConvCNN.CheckingParameters(img_shape, num_classes, conv_shape_list, conv_pool_list,
                                                  conv_relu_list, fc_shape_list, fc_relu_list)
-    lower = 1.e-5
-    upper = 2.e-3
-    ratio = 2.
+    lower = 3.e-4
+    upper = 1.e-2
+    ratio = 1.25
     n_step = 1001
     lrate_list, accu_list = test_model.checkLearningRate(train_set, test_set, lower, upper, ratio, n_step)
     mnist_plot.plotCost(lrate_list, accu_list, xscale = 'log', xlabel = 'Learning rate',
-                        ylabel = 'Test accuracy', file_name = 'main2_results/lr_vs_accu1.pdf')
+                        ylabel = 'Test accuracy', file_name = 'main2_results/lr_vs_accu2.pdf')
+    Optimal learning rate ~ 1.e-3
+    '''
+    
+    running_model = MultiConvCNN.MultiConvLayerCNN(img_shape, num_classes)
+    running_model.constuctCNNetwork(conv_shape_list, conv_pool_list, conv_relu_list, fc_shape_list, fc_relu_list)
+    running_model.initialization(l_rate = 1.e-3, opt_type = 'Adam')
+    running_model.trainData(train_set, 10000, batch_size = 64, output_step = 50)
+    step_list, accu_list, cost_list = running_model.getTrainingResults()
+    
+    # Graphically showing data
+    print("\n\n============Plotting Data============\n\n")
+    mnist_plot.plotCost(step_list , accu_list, ylabel = 'Accuracy', file_name = 'main2_results/accuracy.pdf')
+    mnist_plot.plotCost(step_list , cost_list, ylabel = 'Cost', file_name = 'main2_results/cost.pdf')
+    
+    r = 3
+    c = 4
+    imgs = test_set.images[: r*c]
+    cls = test_set.cls[: r*c]
+    test_accu, cls_pred = running_model.getTestAccuracy(test_set, n_cls_pred = r*c)
+    print("The Final Accuracy is {0>6.2%}".format(test_accu))
+    mnist_plot.plotImage(r, c, imgs, cls, cls_pred = cls_pred, file_name = 'final_prediction_example.pdf')
